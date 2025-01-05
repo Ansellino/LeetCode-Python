e676c2fa355cad33f2c6d55a624f0b4a92eb2d99
AnselZorro <ansellino@gmail.com>
2025-01-06 00:06:16 +0700

    answer 2225

---
+++ 2225 find players with zero or one losses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findWinners(self, matches) :
+        # write code here
+        bracket = {}
+    
+        for winner, loser in matches:
+            bracket[winner] = bracket.get(winner,0)
+            bracket[loser] = bracket.get(loser,0) + 1
+
+        zero_lose = [player for player, lose in bracket.items() if lose == 0]
+        one_lose = [player for player, lose in bracket.items() if lose == 1] 
+
+        zero_lose.sort()
+        one_lose.sort()
+        
+        return [zero_lose, one_lose]
