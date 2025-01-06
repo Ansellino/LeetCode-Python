fc6b2a5b7319ae608fdc75a9a0cb10f0ca4009c7
AnselZorro <ansellino@gmail.com>
2025-01-07 01:17:08 +0700

    add option of 2225

--- 2225 find players with zero or one losses.py
+++ 2225 find players with zero or one losses.py
@@ -14,3 +14,21 @@ class Solution:
         one_lose.sort()
         
         return [zero_lose, one_lose]
+    
+    '''
+    class Solution:
+    def findWinners(self, matches):
+        bracket = {}
+
+        for winner, loser in matches:
+            bracket[winner] = bracket.get(winner, 0)
+            bracket[loser] = bracket.get(loser, 0) + 1
+
+        zero_lose = [player for player, lose in zip(bracket.keys(), bracket.values()) if lose == 0]
+        one_lose = [player for player, lose in zip(bracket.keys(), bracket.values()) if lose == 1]
+
+        zero_lose.sort()
+        one_lose.sort()
+
+        return [zero_lose, one_lose]
+    '''
