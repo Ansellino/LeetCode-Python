adf95082f6d72b8682df20c947b6c1e894864209
AnselZorro <ansellino@gmail.com>
2025-01-06 19:01:22 +0700

    valid sudoku

--- 131-palindrome.py
+++ 131-palindrome.py
@@ -4,6 +4,9 @@ class Solution:
         self.dfs(s, [], res)
         return res
 
+    def isPal(self, s):
+        return s == s[::-1]
+    
     def dfs(self, s, path, res):
         if not s:
             res.append(path)
@@ -12,5 +15,23 @@ class Solution:
             if self.isPal(s[:i]):
                 self.dfs(s[i:], path+[s[:i]], res)
     
+'''
+class Solution:
+    def partition(self, s):
+        res = []
+        self.dfs(s, [], res)
+        return res
+
     def isPal(self, s):
         return s == s[::-1]
+
+    def dfs(self, s, path, res):
+        if not s:
+            res.append(path[:])  # Append a copy of the path to avoid modifying it
+            return
+        for i in range(1, len(s) + 1):
+            if self.isPal(s[:i]):
+                path.append(s[:i])
+                self.dfs(s[i:], path, res)
+                path.pop()  # Backtrack by removing the last added prefix
+'''
